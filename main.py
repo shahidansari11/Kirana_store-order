1ab3cd4241994e3175cc1ddd5846624531c05cce
Shahid Ansari <shahidansari2068@gmail.com>
2025-09-24 13:29:59 +0530

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.header("KGN KIRANA STORE : ")
-
+st.subheader("Irfan Ansari : ")
 st.subheader("Ph No : 9145206349: ")
 st.markdown("Home delivery free:")
 
@@ -23,4 +23,5 @@ if button:
     ,Address : {address}
     ,Ph No : {classdata}""")
 st.markdown("Thank you for visit us : ")
+
 
